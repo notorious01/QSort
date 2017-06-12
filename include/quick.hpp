6b9ab5d06574e41626abe7fe733b737cc29c14f8
notorious01 <tuborg1604@gmail.com>
2017-06-12 21:26:31 +0300

    Update quick.hpp

--- include/quick.hpp
+++ include/quick.hpp
@@ -1,26 +1,23 @@
-#include <iostream> 
-#include <algorithm>
-#include <iterator>
+#include <iostream>
 
-template <class Iterator>
-void quickSort(Iterator left, Iterator right)
+template <typename Iterator>
+void q_Sort(Iterator first, Iterator last)
 {
-	Iterator i=left, j=right;
-	Iterator x = left;
-	while (i <= j)
+	Iterator f = first, l = last;
+	int x = *(first+(last-first)/2);
+	while (f <= l)
 	{
-		while (*i > *x) i++;
-		while (*j < *x) j--;
-		if (i <= j)
+		while (*f < x) f++;
+		while (*l > x) l--;
+		if (f <= l)
 		{
-			std::iter_swap(j, i);
-			i++;
-			j--;
+			std::iter_swap(l, f);
+			f++;
+			l--;
 		}
 	}
-	if (i<right)
-		quickSort(i, right);
-
-	if (left<j)
-		quickSort(left, j);
+	if (f < last)
+		q_Sort( f, last);
+	if (first< l)
+		q_Sort( first, l);
 }
